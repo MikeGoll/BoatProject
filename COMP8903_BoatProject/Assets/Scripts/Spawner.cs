c16d67b496ab659ce3df92c3c3f2386786078188
Michael Goll <mike@Michaels-MacBook-Pro.local>
2018-01-22 10:43:44 -0800

    Marker spawning.

---
+++ COMP8903_BoatProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject marker;
+	private const int MARKERNUM = 38;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = -4; x < MARKERNUM; x++) {
+			GameObject temp = Object.Instantiate(marker, new Vector3(-2, 0, x), Quaternion.identity);
+			temp.GetComponentInChildren<TextMesh>().text = "" + x;
+		}
+	}
+}
