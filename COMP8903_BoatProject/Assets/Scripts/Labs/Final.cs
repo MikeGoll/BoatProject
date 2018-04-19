d54a1ddf33e956aa784f5adbb1b8883f9a360f16
Michael Goll <mike_g97@hotmail.com>
2018-04-19 11:39:12 -0700

    Final project complete.

--- COMP8903_BoatProject/Assets/Scripts/Labs/Final.cs
+++ COMP8903_BoatProject/Assets/Scripts/Labs/Final.cs
@@ -7,8 +7,6 @@ public class Final : MonoBehaviour {
 
 	//---------------------- LAB #7 Additions ----------------------
 	[Header("Lab 7 Attributes - Editable")]
-	[Tooltip("Enables/Disables the 2 second stop. Disable to allow for dynamic acceleration")]
-	public bool dynamicControls;
 	public float dragCoefficient;
 
 	[Header("Lab 7 Calculated")]
@@ -16,40 +14,16 @@ public class Final : MonoBehaviour {
 	public float vMax;
 	public float accelerationWithDrag;
 
-	//---------------------- LAB #6 Additions ----------------------
-	[Header("Lab 6 Attributes - Editable")]
-	//User entered data through UI (force, angle of force, angle, final displacement z, left angular displacement, right angular displacement)
-	public float angularForce;
-	public float angle;
-
 	[Header("Lab 6 Calculated")]
 	public float force;
+
 	[Header("Final Calculated")]
 	public float oMax;
 	public float oldOmega, omega;
 	public float turnForce;
-	private int direction;
-	// public float dinZ;
-	// public float lD;
-	// public float rD;
-
-	//thrust in X and Z axis
-	[Header("Thrust Values")]
-	public Vector3 thrust;
-	public Vector3 thrustAngle;
-	public Vector3 rLeft;
-	public Vector3 rRight;
-	[Header("Torque & Acceleration Values")]
-	public Vector3 torqueL;
-	public Vector3 torqueR;
-	public Vector3 acceleration;
-
-	public float accelerationL, accelerationR;
 
 	[Header("Velocity Values")]
 	//acceleration, initial velocity, final velocity, new intermediate velocity, old intermediate velocity
-	public float v_initial;
-	public float v_final;
 	public float newVx;
 	public float oldVx;
 	public float newVz;
@@ -70,9 +44,6 @@ public class Final : MonoBehaviour {
 	private bool moving, initial, rotLeft, rotRight;
 	private float numUpdates;
 	private float fixedTime;
-	private float angularVelocity, oldAngularVelocity, angularDisplacement, oldAngularDisplacement;
-	private float distanceZ, distanceX;
-	private float temp1;
 
 	//---------------------- LAB #1 Originals ----------------------
 	[Header("Lab 1 Objects")]
@@ -80,44 +51,50 @@ public class Final : MonoBehaviour {
 	public GameObject b;
 	public GameObject p;
 	public GameObject c;
+
 	//masses of the objects
-	[Header("Mass Values")]
-	public float mass_boat;
-	public float mass_pilot;
-	public float mass_cannon;
-	public float mass_com;
+	private float mass_boat;
+	private float mass_pilot;
+	private float mass_cannon;
+	private float mass_com;
+
 	//moments of inertia of the objects
-	[Header("Moment of Inertia Values")]
-	public float moi_boat;
-	public float moi_pilot;
-	public float moi_cannon;
-	public float moi_com;
+	private float moi_boat;
+	private float moi_pilot;
+	private float moi_cannon;
+	private float moi_com;
+
 	//rotation point of the objects
-	[Header("MH Values")]
-	public float mh_boat; 
-	public float mh_pilot;
-	public float mh_cannon;
-	public float mh_com;
+	private float mh_boat; 
+	private float mh_pilot;
+	private float mh_cannon;
+	private float mh_com;
+
 	//inertia totals of the objects
-	[Header("Combined Values")]
-	public float it_boat;
-	public float it_pilot;
-	public float it_cannon;
-	public float it_com;
+	private float it_boat;
+	private float it_pilot;
+	private float it_cannon;
+	private float it_com;
 
 	[Space(20)]
-	public float comX;
-	public float comZ;
+	private float comX;
+	private float comZ;
+
 	//h of the objects
-	public float h_boat, h_pilot, h_cannon;
+	private float h_boat, h_pilot, h_cannon;
+
 	//pilot object used to obtain pilot data
 	private Pilot pilot;
+
 	//pilot boat used to obtain boat data
 	private Boat boat;
+
 	//pilot boat used to obtain cannon data
 	private Cannon cannon;
+
 	//the size of the data arrays to update the HUD
 	private int arraySize;
+
 	//the material used to display the COM point
 	public Material comPoint;
 
@@ -136,20 +113,10 @@ public class Final : MonoBehaviour {
 -- Initializes the data arrays.
 ----------------------------------------------------------------------------------------------------------------------*/
 	void Start () {
-		//------- LAB #6 Additions -------
 		moving = true;
 		initial = true;
 		numUpdates = 0;
 		fixedTime = Time.fixedDeltaTime;
-
-		oldVz = v_initial;
-
-		//calculate different acceleration directions
-		thrust.x = PhysicsCalculator.calculateXThrust(force, angle);
-		thrust.z = PhysicsCalculator.calculateZThrust(force, angle);
-
-		thrustAngle.x = PhysicsCalculator.calculateXThrust(angularForce, angle);
-		thrustAngle.z = PhysicsCalculator.calculateZThrust(angularForce, angle);
 	}
 	
 /*------------------------------------------------------------------------------------------------------------------
@@ -167,37 +134,39 @@ public class Final : MonoBehaviour {
 -- Updates the COM Point material to move the com point shader around.
 ----------------------------------------------------------------------------------------------------------------------*/
 	void Update () {
-		//------- LAB #6 Additions -------
-		// acceleration = PhysicsCalculator.calculateAccelerationFromThrust(force, mass_com);
 
+		//Pause/unpause simulation
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			moving = !moving;
 		}
 
+		//Forward Thrust
 		if (Input.GetKeyDown(KeyCode.W)) {
 			force = 4000;
 		}
 
-		if (Input.GetKeyUp(KeyCode.W) && dynamicControls) {
+		//Release forward thrust
+		if (Input.GetKeyUp(KeyCode.W)) {
 			force = 0;
 		}
 
+		//Left torque
 		if (Input.GetKeyDown(KeyCode.Q)) {
 			rotLeft = true;
 			rotRight = false;
 
-			turnForce = 40000;
-			direction = -1;
+			turnForce = -1000;
 		}
 
+		//Right torque
 		if (Input.GetKeyDown(KeyCode.E)) {
 			rotRight = true;
 			rotLeft = false;
 
-			turnForce = 40000;
-			direction = 1;
+			turnForce = 1000;
 		}
 
+		//Release directional torque
 		if (Input.GetKeyUp(KeyCode.E) || Input.GetKeyUp(KeyCode.Q)) {
 			rotRight = false;
 			rotLeft = false;
@@ -206,7 +175,7 @@ public class Final : MonoBehaviour {
 		}
 
 		if (initial) {
-			//------- LAB #1 Originals -------
+			//Initialize objects to access attributes
 			pilot = p.GetComponent<Pilot>();
 			boat = b.GetComponent<Boat>();
 			cannon = c.GetComponent<Cannon>();
@@ -223,25 +192,6 @@ public class Final : MonoBehaviour {
 			calculateInertiaTotals();
 
 			comPoint.SetVector("_COMPosition", new Vector3(comX, 0, comZ));
-
-			//------- LAB #6 Additions -------
-			acceleration.x = PhysicsCalculator.calculateAccelerationFromThrust(thrust.x, mass_com);
-			acceleration.z = PhysicsCalculator.calculateAccelerationFromThrust(thrust.z, mass_com);
-
-			rLeft.x = 2 - comX;
-			rLeft.y = 0;
-			rLeft.z = -4 - comZ; 
-
-			rRight.x = -2 - comX;
-			rRight.y = 0;
-			rRight.z = -4 - comZ;
-
-			torqueL = PhysicsCalculator.calculateCrossProd(new Vector3(thrustAngle.x, 0, thrustAngle.z), rLeft);
-			torqueR = PhysicsCalculator.calculateCrossProd(new Vector3(thrustAngle.x, 0, thrustAngle.z), rRight);
-
-			accelerationL = PhysicsCalculator.calculateAngularAcceleration(torqueL.y, it_com);
-			accelerationR = PhysicsCalculator.calculateAngularAcceleration(torqueR.y, it_com);
-
 			initial = false;
 		}
 	}
@@ -250,44 +200,27 @@ public class Final : MonoBehaviour {
 	void FixedUpdate() {
 		if (moving) {
 
-			if (numUpdates == 100 && !dynamicControls) {
-				moving = false;
-			}
-
-			//calculate the new distances and velocities
-			newDx = PhysicsCalculator.calculateDistance(oldDx, acceleration.x, oldVx, fixedTime);
-			newVx = PhysicsCalculator.calculateVelocity(oldVx, acceleration.x, fixedTime);
-
-			newDz = PhysicsCalculator.calculateDistance(oldDz, acceleration.z, oldVz, Time.fixedDeltaTime);
-			newVz = PhysicsCalculator.calculateVelocity(oldVz, acceleration.z, Time.fixedDeltaTime);
-
-			angularVelocity = oldAngularVelocity + (temp1 * fixedTime);
-
-			angularDisplacement += Mathf.Abs(angularVelocity * fixedTime);
-
-			distanceX += newVx * fixedTime;
-			distanceZ += newVz * fixedTime;
-
-
-			//------- LAB #7 Additions -------
+			//Calculate tau dynamically to account for mass changes
 			tau = PhysicsCalculator.calculateTau(mass_com, dragCoefficient);
-			vMax = PhysicsCalculator.calculteTerminalVelocity(force, dragCoefficient);
 
+			//Calculate the boat's velocity for its forward motion
+			vMax = PhysicsCalculator.calculteTerminalVelocity(force, dragCoefficient);
 			newVz = PhysicsCalculator.calculateVelocityWithDrag(vMax, dragCoefficient, oldVz, fixedTime, mass_com);
 			newDz = PhysicsCalculator.calculatePositionWithDrag(oldDz, force, dragCoefficient, fixedTime, mass_com, oldVz);
 
+			//Calculate the linear movement acceleration with drag
 			accelerationWithDrag = PhysicsCalculator.calculateAccelerationWithDrag(force, dragCoefficient, oldVz, mass_com);
 
-			totalBoat.transform.Translate(newVx * fixedTime, 0, newVz * fixedTime);
-
+			//Calculate the boat's velocity and acceleration with drag for its angular motion
 			oMax = PhysicsCalculator.calculateTerminalAngularVelocity(turnForce, dragCoefficient);
 			omega = PhysicsCalculator.calculateAngularVelocityWithDrag(oMax, dragCoefficient, oldOmega, fixedTime, mass_com);
 
-			totalBoat.transform.Rotate(0, direction * omega * fixedTime, 0, Space.Self);
+			//Update the boat's position
+			totalBoat.transform.Translate(newVx * fixedTime, 0, newVz * fixedTime);
+			totalBoat.transform.Rotate(0, Mathf.Rad2Deg * (omega * fixedTime), 0, Space.Self);
 		}
 
 		if (moving) {
-
 			numUpdates++;
 			 
 			//update UI
@@ -298,15 +231,10 @@ public class Final : MonoBehaviour {
 			
 			oldDx = newDx;
 			oldVx = newVx;
-
 			oldDz = newDz;
 			oldVz = newVz;
-			oldAngularVelocity = angularVelocity;
-
+			
 			oldOmega = omega;
-
-		} else {
-			v_final = oldVz;
 		}
 	}
 
